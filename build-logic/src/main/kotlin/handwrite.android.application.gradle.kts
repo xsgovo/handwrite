cc9090b2900dc7e838b1b99199470224cbd31546
xsgovo <xiaosiguaa@gmail.com>
2026-07-19 06:03:32 +0800

    build: shrink release artifacts

--- build-logic/src/main/kotlin/handwrite.android.application.gradle.kts
+++ build-logic/src/main/kotlin/handwrite.android.application.gradle.kts
@@ -11,6 +11,14 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"))
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
